169dca7f22fb364c58045f4931951b10c0bec871
Karo Yousefi <karoyousefi.80@gmail.com>
2025-04-22 11:34:09 +0330

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-tailwind-todo-list/"
+  base: "/react-tailwind-todo-list/",
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
